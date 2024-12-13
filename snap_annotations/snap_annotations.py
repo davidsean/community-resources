2abbf3dbbb40ee99b54a9876da3fddc63cac5a85
davidsean <david.sean@gmail.com>
2024-12-13 15:19:32 -0500

    add biigle.py, add frame is not frame but time!

--- snap_annotations/snap_annotations.py
+++ snap_annotations/snap_annotations.py
@@ -2,9 +2,12 @@
 from biigle import Api
 
 # Biigle email and token
+
 email = 'ENTER EMAIL HERE'
 token = 'ENTER TOKEN HERE'
 video_id = 123456789
+video_frame_rate = 123456789 # TODO: implement get_video_fps(video_id) to automatically get the framerate
+
 
 
 def update_annotation_frames(annotation_id, new_frames, points):
@@ -15,6 +18,22 @@ def update_annotation_frames(annotation_id, new_frames, points):
     res = api.put('video-annotations/{}'.format(annotation_id), json=payload)
     if res.status_code != 200:
         print("Unable to snap annotation time for annotation {}".format(annotation_id))
+
+def snap_timeframe(timestamp:float, fps:float):
+    '''
+    note: biigle_frame is the timestamp in seconds (not a frame)
+    '''
+    frame = timestamp*fps
+    snapped_timestamp = int(frame)/fps
+    return snapped_timestamp
+
+def get_video_fps(video_id:int) -> float:
+    '''
+    TODO:
+    prefetch video until moov atom is complete and return framerate
+    '''
+    raise NotImplementedError
+
 
 
 api = Api( email=email, token=token)
@@ -27,8 +46,11 @@ for annotation in res.json():
     frames = annotation.get('frames', None)
     points = annotation.get('points', None)
     annotation_id = annotation.get('id', None)
+    print(annotation)
     if frames and points and annotation_id:
         new_frames = []
         for frame in frames:
-            new_frames.append(int(frame))
+            new_frame = snap_timeframe(frame, video_frame_rate)
+            print(f"will update ts={frame} to ts={new_frame}") 
+            new_frames.append(new_frame)
         update_annotation_frames(annotation_id, new_frames, points)
